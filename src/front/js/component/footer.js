d108efeb8b8137318a49ecb00afca4328d8a854f
Jovany1543 <christian.marrero3@gmail.com>
2023-02-28 20:27:21 +0000

    added chatbox

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ChatBox from "./chatBox";
+export const Footer = (props) => {
+  const [showOffCanvas, setShowOffCanvas] = useState(false);
+  let Navigate = useNavigate();
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+  const toggleChat = () => {
+    props.setShowChat(!props.showChat);
+  };
+
+  return (
+    <nav className="navbar fixed-bottom bg-light ">
+      <div className="container-fluid">
+        <button
+          onClick={() => setShowOffCanvas(true)}
+          className="navbar-toggler bg-light"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <i class="fa-solid fa-users-between-lines"></i>
+        </button>
+        <div>
+          <button
+            onClick={toggleChat}
+            class="btn navbar-toggler bg-light"
+            type="button"
+            //   data-bs-toggle="offcanvas"
+            //   data-bs-target="#offcanvasBottom"
+            //   aria-controls="offcanvasBottom"
+          >
+            <i class="fa-regular fa-comments"></i>
+          </button>
+        </div>
+
+        {/* <div
+          class="offcanvas offcanvas-bottom"
+          tabindex="-1"
+          id="offcanvasBottom"
+          aria-labelledby="offcanvasBottomLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasBottomLabel">
+              Chat
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="offcanvas-body small">
+            <ChatBox />
+          </div>
+        </div> */}
+      </div>
+    </nav>
+  );
+};
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
+
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
+import { ChatBox } from "./component/chatBox";
 import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  const [showChat, setShowChat] = useState(false);
+
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+
+            <Route element={<Single />} path="/single/:theid" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          {showChat && (
+            <div className="chat fixed-bottom">
+              <ChatBox />
+            </div>
+          )}
+          <Footer showChat={showChat} setShowChat={setShowChat} />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
